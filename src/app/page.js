f58a321e69a68ad3fb12db0433240528b1eb7fa8
Thomas-Gonz67 <thomasgonz67@gmail.com>
2024-07-31 00:22:59 -0400

    added call to action button

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import MailChimp from '../Components/mailchimp.js';
+import { Button } from '@mui/material';
 
 export default function Home() {
   return (
       <main className={styles.main}>
           <div>
-              <p>hello</p>
+              <Button
+                  style={{
+                      backgroundColor: '#A3A791',  // Custom hex color
+                      color: '#2C0000',           // Custom text color
+                      fontFamily: 'Ribeye Marrow, sans-serif', // Custom font
+                      borderRadius: '100px',      // Large border radius
+                      fontSize: '24px',
+                      padding: '10px 20px',
+                  }}
+              >
+        JOIN TODAY
+        </Button>
               <MailChimp/>
           </div>
     </main>
